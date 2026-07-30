7c410a452c930d03b746c9b2ef32ae98da9e0691
9rey-m477er <9rey-m477er@github.com>
2026-07-30 22:59:51 +0000

    Test run: 19/28 passed, 9 failed, 0 skipped - 2026-07-30T22:59:51.103Z

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.entity.Account;
 import com.example.entity.Message;
@@ -45,5 +46,11 @@ public class SocialMediaController {
       return ResponseEntity.ok().build();
     }
     return ResponseEntity.ok(value);
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<Account> register(@RequestBody Account ac){
+    Account newAccount = as.addAccount(ac);
+    return ResponseEntity.ok(newAccount);
   }
 }
